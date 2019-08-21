7eda73b8acc0603cc815301e091c671c9894ba96
Jonathan Perry <jpper@umich.edu>
2019-08-21 11:41:46 +0900

    Commit of simon's maps tracking app

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,129 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
+import { Plugins } from '@capacitor/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
+const { Geolocation } = Plugins;
+
+declare var google;
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  locations: Observable<any>;
+  locationsCollection: AngularFirestoreCollection<any>;
+  user = null;
+
+  @ViewChild('map', { static: false }) mapElement: ElementRef;
+  map: any;
+  markers = [];
+
+  isTracking = false;
+  watch = null;
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private afs: AngularFirestore,
+  ) {
+    this.anonLogin();
+  }
+
+  ionViewWillEnter() {
+    this.loadMap();
+  }
+
+  loadMap() {
+    let latLng = new google.maps.LatLng(51.9036442, 7.6673267);
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 5,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+  }
+
+  anonLogin() {
+    this.afAuth.auth.signInAnonymously().then(res => {
+      this.user = res.user;
+
+      this.locationsCollection = this.afs.collection(
+        `locations/${this.user.uid}/track`,
+        ref => ref.orderBy('timestamp')
+      );
+
+      // load firebase data
+      this.locations = this.locationsCollection.snapshotChanges().pipe(
+        map(actions =>
+          actions.map(a => {
+            const data = a.payload.doc.data();
+            const id = a.payload.doc.id;
+            return { id, ...data };
+          })
+        )
+      );
+
+      // update map
+      this.locations.subscribe(locations => {
+        console.log('new locations: ', locations);
+        this.updateMap(locations);
+      });
+    });
+  }
+
+  updateMap(locations) {
+    this.markers.map(marker => marker.setMap(null));
+    this.markers = [];
+
+    for (let loc of locations) {
+      let latLng = new google.maps.LatLng(loc.lat, loc.lng);
+
+      let marker = new google.maps.Marker({
+        position: latLng,
+        animation: google.maps.Animation.DROP,
+        map: this.map
+      });
+      this.markers.push(marker);
+    }
+  }
+
+  startTracking() {
+    this.isTracking = true;
+    this.watch = Geolocation.watchPosition({}, (position, err) => {
+      if (position) {
+        this.addNewLocation(
+          position.coords.latitude,
+          position.coords.longitude,
+          position.timestamp
+        );
+      }
+    });
+  }
+
+  stopTracking() {
+    Geolocation.clearWatch({ id: this.watch }).then(() => {
+      this.isTracking = false;
+    });
+  }
+
+  addNewLocation(lat, lng, timestamp) {
+    this.locationsCollection.add({
+      lat,
+      lng,
+      timestamp
+    });
+    let position = new google.maps.LatLng(lat, lng);
+    this.map.setCenter(position);
+    this.map.setZoom(5);
+  }
+
+  deleteLocation(pos) {
+    this.locationsCollection.doc(pos.id).delete();
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDtnZ-EvL0pzGNBtldkQ4rn3OjlTtOOwOQ",
+    authDomain: "location-tracker-5f182.firebaseapp.com",
+    databaseURL: "https://location-tracker-5f182.firebaseio.com",
+    projectId: "location-tracker-5f182",
+    storageBucket: "",
+    messagingSenderId: "845414493940",
+    appId: "1:845414493940:web:5ae3a15839d39651"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
